5a2581b627c6324f7ada53add37f6d02b19d0a5c
Morgan Hepler <morganheplerr@gmail.com>
2025-11-04 12:13:09 -0600

    imported html and javascript

---
+++ script.js
@@ -0,0 +1,77 @@
+// DOM Elements
+const creatureAPI = "https://rpg-creature-api.freecodecamp.rocks/api/creatures";
+const searchBtn = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+const creatureName = document.getElementById("creature-name");
+const creatureID = document.getElementById("creature-id");
+const creatureWeight = document.getElementById("weight");
+const creatureHeight = document.getElementById("height");
+const creatureTypes = document.getElementById("types");
+const creatureSpecial = document.getElementById("special");
+const creatureHP = document.getElementById("hp");
+const creatureAttack = document.getElementById("attack");
+const creatureDefense = document.getElementById("defense");
+const creatureSpecialAttack = document.getElementById("special-attack");
+const creatureSpecialDefense = document.getElementById("special-defense");
+const creatureSpeed = document.getElementById("speed");
+
+// Reset Display
+const resetDisplay = () => {
+  searchInput.value = '';
+  creatureName.textContent = '';
+  creatureID.textContent = '';
+  creatureWeight.textContent = '';
+  creatureHeight.textContent = '';
+  creatureTypes.innerHTML = '';
+  creatureSpecial.textContent = '';
+  creatureHP.textContent = '';
+  creatureAttack.textContent = '';
+  creatureDefense.textContent = '';
+  creatureSpecialAttack.textContent = '';
+  creatureSpecialDefense.textContent = '';
+  creatureSpeed.textContent = '';
+}
+
+// Display Creature Info
+const displayCreatureInfo = (data) => {
+  const { id, name, weight, height, special, stats, types } = data;
+  resetDisplay();
+
+  // Update DOM Elements
+  creatureName.textContent = name;
+  creatureID.textContent = ` #${id}`;
+  creatureWeight.textContent = `Weight: ${weight}`;
+  creatureHeight.textContent = ` Height: ${height}`;
+
+  creatureTypes.innerHTML = types.map(type => `<span>${type.name}</span>`).join("");
+
+  creatureSpecial.innerHTML = `
+    <p>${special.name}</p>
+    <p id="${special.name}">${special.description}</p>
+  `;
+
+  creatureHP.textContent = stats[0].base_stat;
+  creatureAttack.textContent = stats[1].base_stat;
+  creatureDefense.textContent = stats[2].base_stat;
+  creatureSpecialAttack.textContent = stats[3].base_stat;
+  creatureSpecialDefense.textContent = stats[4].base_stat;
+  creatureSpeed.textContent = stats[5].base_stat;
+
+}
+
+// Fetch Creature
+const fetchCreature = async () => {
+  try {
+    const creatureInput = searchInput.value.toLowerCase();
+    const response = await fetch(`https://rpg-creature-api.freecodecamp.rocks/api/creature/${creatureInput}`);
+    const data = await response.json();
+    displayCreatureInfo(data);
+  }
+  catch (err) {
+    resetDisplay();
+    alert('Creature not found');
+    console.log(err);
+  }
+}
+
+searchBtn.addEventListener("click", fetchCreature);
